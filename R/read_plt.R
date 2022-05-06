a41736b71621425a04113452500669b00edc9135
David Holstius <david.holstius@gmail.com>
2022-05-06 12:27:32 -0700

    support terra and sf return types

--- R/read_plt.R
+++ R/read_plt.R
@@ -14,6 +14,8 @@
 #' @importFrom stringr str_sub str_extract_all
 #' @importFrom vroom vroom_fwf
 #' @importFrom readr read_lines
+#' @importFrom terra rast
+#' @importFrom sf st_as_sf st_crs
 #'
 #' @return an object of type `as` (default: `tbl`)
 #'
@@ -22,9 +24,10 @@
 #' @export
 read_plt <- function (
   path,
-  as = c("tbl", "tbl_cube", "array", "data.table", "raster"),
+  as = c("tbl", "tbl_cube", "array", "data.table", "raster", "terra", "sf"),
   cols = c(X = "X", Y = "Y", CONC = "AVERAGE CONC"),
   complete = TRUE,
+  crs = NULL,
   ...,
   verbose = getOption("verbose", default = FALSE)
 ) {
@@ -47,7 +50,21 @@ read_plt <- function (
     plt_dt <- read_plt(path, as = "data.table", cols = cols, ..., verbose = verbose)
     result <- xy2array.data.table(plt_dt)
   } else if (as == "raster") {
-    result <- read_plt_as_raster(path, ..., verbose = verbose)
+    rst_obj <- read_plt_as_raster(path, ..., crs = crs, verbose = verbose)
+    result <- rst_obj
+  } else if (as == "terra") {
+    rst_obj <- read_plt_as_raster(path, ..., crs = crs, verbose = verbose)
+    terra_obj <- terra::rast(rst_obj)
+    result <- terra_obj
+  } else if (as == "sf") {
+    plt_tbl <- read_plt_vroom_fwf(path, cols = cols, ..., verbose = verbose)
+    sf_obj <- sf::st_as_sf(plt_tbl, coords = cols[c("X", "Y")])
+    if (isFALSE(is.null(crs))) {
+      sf::st_crs(sf_obj) <- crs
+    } else {
+      warning("[read_plt] crs is NULL")
+    }
+    result <- sf_obj
   } else {
     err_msg <- "not yet supported"
     stop(err_msg)
